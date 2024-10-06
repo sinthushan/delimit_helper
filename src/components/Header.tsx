e75a7684b583c33f657b80ce810e79c16c60bf5b
sinthushan <sinthushan@gmail.com>
2024-10-05 23:01:27 -0400

    changing title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="header">
-      <h1>Delimiter Help</h1>
+      <h1>Text To Column</h1>
       <p>A service to quickly split text</p>
     </header>
   );
